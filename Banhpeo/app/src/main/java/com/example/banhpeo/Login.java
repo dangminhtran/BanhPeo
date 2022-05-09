4d2b386191bf8c42531265a61cde78a4d92fd0c2
Dang Tran <trmdang@my.yorku.ca>
2022-05-09 11:27:02 -0400

    Update Login.java

--- Banhpeo/app/src/main/java/com/example/banhpeo/Login.java
+++ Banhpeo/app/src/main/java/com/example/banhpeo/Login.java
@@ -11,6 +11,5 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
     }
 }
