87f5df6a751279f102d9e2db457b9064ffbd0af5
tranguv <89886131+tranguv@users.noreply.github.com>
2022-05-15 21:02:36 +0700

    ee

--- Banhpeo/app/src/main/java/com/example/banhpeo/Mainhome.java
+++ Banhpeo/app/src/main/java/com/example/banhpeo/Mainhome.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
