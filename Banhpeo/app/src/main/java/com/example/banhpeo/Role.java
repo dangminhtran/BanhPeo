8a41e09c63f171db4ff525e61340e05357dea2b6
Dang Tran <trmdang@my.yorku.ca>
2022-05-26 16:01:35 +0700

    Update Role.java

--- Banhpeo/app/src/main/java/com/example/banhpeo/Role.java
+++ Banhpeo/app/src/main/java/com/example/banhpeo/Role.java
@@ -24,8 +24,8 @@ public class Role extends AppCompatActivity implements AdapterView.OnItemSelecte
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.role);
-        setContentView(R.layout.home_teacher);
+        setContentView(R.layout.role);
+//        setContentView(R.layout.home_teacher);
 
     }
 
